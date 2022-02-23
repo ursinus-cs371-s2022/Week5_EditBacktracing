6037986e3ecd6259160c2478e04c800534b26fa4
Chris Tralie <chris.tralie@gmail.com>
2022-02-22 23:20:48 -0500

    Huh

--- edit.py
+++ edit.py
@@ -4,6 +4,55 @@ LEFT = 0
 UP = 1
 DIAG = 2
 
+def backtrace(s1, s2, moves, path, solution = []):
+    """
+    Recursively backtrace through the arrows, and branch off
+    if there are multiple options (ties) at particular locations
+
+    Parameters
+    ----------
+    s1: string(M)
+        First string
+    s2: string(N)
+        Second string
+    moves: 3d array (M, N, [])
+        A 2D array of lists of possible moves
+    path: list of [i, j]
+        A stack holding the path being recursively built
+    solution: list of string
+        A verbal description of the solution
+    """
+    [i, j] = path[-1]
+    if i == 0 and j == 0:
+        print("SOLUTION")
+        for s in solution[::-1]:
+            print("\t", s)
+    else:
+        for move in moves[i][j]:
+            # Forks in the road (possible arrows to visit)
+            inew = i
+            jnew = j
+            if move == LEFT:
+                jnew -= 1
+                solution.append("Adding {} to s1 (+1)".format(s2[jnew]))
+            elif move == UP:
+                inew -= 1
+                solution.append("Deleting {} from s1 (+1)".format(s1[inew]))
+            else:
+                inew -= 1
+                jnew -= 1
+                if s1[i-1] != s2[j-1]:
+                    solution.append("Swapping in {} for {} in s1 (+1)".format(s2[j-1], s1[i-1]))
+                else:
+                    solution.append("Matching {}".format(s2[j-1]))
+            # Add this option
+            path.append([inew, jnew])
+            # recursively explore the next choices after this
+            backtrace(s1, s2, moves, path, solution)
+            # Pop off this option and try something else
+            path.pop()
+            solution.pop()
+
 def edit(s1, s2):
     """
     An iterative, dynamic programming version of the string
@@ -32,7 +81,8 @@ def edit(s1, s2):
     table[0, :] = np.arange(N+1)
     table[:, 0] = np.arange(M+1)
 
-    # Make 2D array that stores the optimal moves
+    # Make 2D array that stores a list of optimal moves.  There
+    # will be multiple elements in the list if there are ties
     moves = []
     for i in range(M+1):
         moves.append([])
@@ -60,31 +110,6 @@ def edit(s1, s2):
             if table[i][j] == cost3:
                 moves[i][j].append(DIAG)
     
-    print(moves)
-    """
-    ## TODO: Extract an optimal sequence of moves.
-    ## Backtrace from i = M, j = N, following the arrows, until you get to [0, 0]
-    i = M
-    j = N
-    path = []
-    while not (i == 0 and j == 0):
-        if moves[i][j] == LEFT:
-            path.append("Adding {} to s1".format(s2[j-1]))
-            j -= 1
-        elif moves[i][j] == UP:
-            path.append("Deleting {} from s1".format(s1[i-1]))
-            i -= 1
-        else:
-            if s1[i-1] != s2[j-1]:
-                path.append("Swapping in {} for {} in s1".format(s2[j-1], s1[i-1]))
-            else:
-                path.append("Matching {}".format(s2[j-1]))
-            i -= 1
-            j -= 1
-    path.reverse()
-    for step in path:
-        print(step)
-    return cost
-    """
+    backtrace(s1, s2, moves, [[M, N]])
 
 edit("school", "fools")
